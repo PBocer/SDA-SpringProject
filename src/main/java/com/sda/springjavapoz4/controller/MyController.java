54f1c861121a3729ccfff9a82a9fede3617a32fd
PBocer <przemek.bocer@gmail.com>
2017-11-07 22:11:31 +0100

    post handling

--- src/main/java/com/sda/springjavapoz4/controller/MyController.java
+++ src/main/java/com/sda/springjavapoz4/controller/MyController.java
@@ -1,9 +1,9 @@
 package com.sda.springjavapoz4.controller;
 
-
 import com.sda.springjavapoz4.service.RandomNumbersGeneratorService;
 import com.sda.springjavapoz4.service.SomeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,7 +18,7 @@ public class MyController {
     @Autowired
     private SomeService someService;
 
-    @GetMapping("/users")
+    @GetMapping("/userspoinluhljkh")
     public ModelAndView users() {
         System.out.println("blabla");
         someService.someAction();
